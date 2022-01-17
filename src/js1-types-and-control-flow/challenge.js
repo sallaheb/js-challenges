e0b7d278e1058ccf515f95b9052a5a39b9bbff08
Ebrima Sallah <sallahebrima@ymail.com>
2022-01-17 14:55:45 +0000

    js challenge

--- src/js1-types-and-control-flow/challenge.js
+++ src/js1-types-and-control-flow/challenge.js
@@ -19,7 +19,7 @@ const firstName = "John";
 const lastName = "Smith";
 
 export const createFullName = () => {
-  // Write your code here
+  return firstName +" "+ lastName 
 };
 
 /**
@@ -32,7 +32,11 @@ const largeNumber1 = 100;
 const largeNumber2 = 200;
 
 export const findLargestNumber = () => {
-  // Write your code here
+  if (largeNumber1 > largeNumber2) {
+  return largeNumber1
+  } else  { 
+    return largeNumber2
+  }
 };
 
 /**
@@ -45,7 +49,7 @@ const addNumber1 = 12;
 const addNumber2 = 24;
 
 export const addNumbers = () => {
-  // Write your code here
+ return addNumber1 + addNumber2
 };
 
 /* Intermediate Challenges */
@@ -59,7 +63,7 @@ export const addNumbers = () => {
 const password = "thisIsMyVeryLongPassword123456789";
 
 export const findLengthOfPassword = () => {
-  // Write your code here
+  return password.length
 };
 
 /**
@@ -75,7 +79,15 @@ export const findLengthOfPassword = () => {
 const thing = "I am a thing";
 
 export const findType = () => {
-  // Write your code here
+  if (typeof thing === "string") {
+    return "This is a string"
+  } else if (typeof thing === "number") {
+    return "This is a number"
+  } else if (typeof thing === "boolean") {
+    return "This is a number" 
+  } else {
+    return "I don't know what this thing is"
+  }
 };
 
 /**
@@ -88,7 +100,11 @@ export const findType = () => {
 const nameTagOption = "Timothy";
 
 export const getIsValidOnNameTag = () => {
-  // Write your code here
+  if (nameTagOption.length <= 8 && nameTagOption.charAt(0) === nameTagOption.charAt(0).toUpperCase()) {
+    return true 
+  } else {
+    return false
+  }
 };
 
 /* Advanced Challenges */
